2a55d21a7274ed2029859d95997b9ef37e44b7b0
Magnar Eivind Martinsen <magnar.eivind.martinsen@met.no>
2025-11-27 20:49:16 +0100

    solving with defining custom SolrDist Copy paste from dmci

--- catalog_rebuilder.py
+++ catalog_rebuilder.py
@@ -36,10 +36,11 @@ from celery.result import GroupResult, ResultBase
 from celery.utils.log import get_task_logger
 from dmci.api.app import App
 from dmci.api.worker import Worker as DmciWorker
-from dmci.distributors import PyCSWDist, SolRDist
+from dmci.distributors import PyCSWDist
+from dmci.distributors.distributor import DistCmd, Distributor
 from lxml import etree
 from requests.auth import HTTPBasicAuth
-from solrindexer.indexdata import IndexMMD
+from solrindexer.indexdata import IndexMMD, MMD4SolR
 
 from main import CRConfig
 
@@ -86,6 +87,7 @@ indexMMD = IndexMMD(CRCONFIG.solr_service_url, always_commit=False,
 class CRPyCSWMDist(PyCSWDist):
     """Override PyCSwDist  with the given config read from rebuilder"""
     dmci.CONFIG = CRCONFIG
+
     def __init__(self, cmd, xml_file=None, metadata_UUID=None, worker=None, **kwargs):
         dmci.CONFIG = CRCONFIG
         super().__init__(cmd, xml_file, metadata_UUID, worker, **kwargs)
@@ -93,22 +95,144 @@ class CRPyCSWMDist(PyCSWDist):
         return
 
 
-class CRSolrDist(SolRDist):
+class CRSolrDist(Distributor):
     """Override SolRDist  with the given config read from rebuilder"""
     dmci.CONFIG = CRCONFIG
+    TOTAL_DELETED = "total_deleted"
+    TOTAL_INSERTED = "total_inserted"
+    TOTAL_UPDATED = "total_updated"
+    STATUS = [TOTAL_DELETED, TOTAL_INSERTED, TOTAL_UPDATED]
+
     def __init__(self, cmd, xml_file=None, metadata_UUID=None, worker=None, **kwargs):
         dmci.CONFIG = CRCONFIG
         self._conf = CRCONFIG
         super().__init__(cmd, xml_file, metadata_UUID, worker, **kwargs)
-        logger.debug(f"CRSolrDistConstructor_ {self._conf.solr_service_url}")
-        # self._conf.fail_on_missing_parent = False
-        #self.authentication = self._init_authentication()
         self._conf = CRCONFIG
+        logger.debug(f"CRSolrDistConstructor_ {self._conf.solr_service_url}")
 
-        #  Use the initiilized solr connection
-        self.mysolr = indexMMD
-        self.mysolr.solr_url = CRCONFIG.solr_service_url
+        self.authentication = self._init_authentication()
+
+        #  Create connection to solr
+        self.mysolr = IndexMMD(self._conf.solr_service_url, always_commit=False,
+                               authentication=self.authentication, config={})
         return
+
+    def _init_authentication(self):
+        if self._conf.solr_username is not None and self._conf.solr_password is not None:
+            return HTTPBasicAuth(self._conf.solr_username,
+                                 self._conf.solr_password)
+        return None
+
+    def run(self):
+        """Run function to handle insert, update or delete
+
+        Returns
+        -------
+        status : bool
+            True if successful insert, update or delete
+            False if not
+        """
+        status = False
+        msg = "No job was run"
+        if not self.is_valid():
+            return False, "The run job is invalid"
+
+        if self._cmd == DistCmd.UPDATE:
+            status, msg = self._add()
+        elif self._cmd == DistCmd.DELETE:
+            status, msg = self._delete()
+        elif self._cmd == DistCmd.INSERT:
+            status, msg = self._add()
+
+        return status, msg
+
+    def _add(self):
+        """Index to SolR."""
+        try:
+            mydoc = MMD4SolR(filename=self._xml_file)
+        except Exception as e:
+            msg = "Could not read file %s: %s" % (self._xml_file, str(e))
+            logger.error(msg)
+            return False, msg
+
+        mydoc.check_mmd()  # Determine if we need to run this check.
+
+        """ Convert mmd file to solr format """
+        try:
+            newdoc = mydoc.tosolr()
+        except Exception as e:
+            msg = 'Could not process the file %s: %s' % (
+                self._xml_file, str(e))
+            logger.error(msg)
+            return False, msg
+
+        """Check if document already exsists. Then we throw error and don't index."""
+        isIndexed = self.mysolr.get_dataset(newdoc['id'])
+
+        if isIndexed is not None:
+            if isIndexed['doc'] is not None and self._cmd == DistCmd.INSERT:
+                msg = "Document already exists in index, %s" % newdoc['metadata_identifier']
+                logger.error(msg)
+                return False, msg
+
+            if 'related_dataset' in newdoc:
+                logger.debug("Child dataset with id: %s",
+                             newdoc['metadata_identifier'])
+                logger.debug("Child dataset's parent's id is: %s",
+                             newdoc['related_dataset'])
+                parentid = newdoc['related_dataset_id']
+                try:
+                    status, msg = self.mysolr.update_parent(
+                        parentid,
+                        fail_on_missing=self._conf.fail_on_missing_parent
+                    )
+                except Exception as e:
+                    msg = "Failed to update parent in SolR.Reason: %s" % str(e)
+                    logger.error(msg)
+                    return False, msg
+
+                if status:
+                    status, msg = self._index_record(
+                        newdoc, add_thumbnail=False, level=2)
+                else:
+                    return status, msg
+            else:
+                logger.debug("Parent/Level-1 - dataset - %s",
+                             newdoc['metadata_identifier'])
+                status, msg = self._index_record(
+                    newdoc, add_thumbnail=False, level=1)
+        else:
+            msg = "Failed to insert dataset in SolR."
+            logger.error(msg)
+            return False, msg
+        return status, msg
+
+    def _index_record(self, newdoc, add_thumbnail=False, level=1):
+        """ Wrapper function to return correct parameters (status and msg).
+        """
+        try:
+            status, msg = self.mysolr.index_record(
+                newdoc, addThumbnail=add_thumbnail, level=level)
+            logger.info("Indexed document %s in SolR"
+                        % newdoc['metadata_identifier'])
+        except Exception as e:
+            msg = "Could not index file %s, in SolR. Reason: %s" % (
+                self._xml_file, str(e))
+            logger.error(msg)
+            status = False
+        return status, msg
+
+    def _delete(self):
+        """Delete entry with a specified metadata_id."""
+        identifier = self._construct_identifier(self._worker._namespace,
+                                                self._metadata_UUID)
+        status, msg = self.mysolr.delete(
+            identifier, commit=self._conf.commit_on_delete)
+        logger.info("SolR delete status: %s. With response: %s" %
+                    (str(status), str(msg)))
+        return status, msg
+
+# END Class SolRDist
 
 
 class Worker(DmciWorker):
@@ -118,6 +242,7 @@ class Worker(DmciWorker):
         "solr": CRSolrDist
     }
     dmci.CONFIG = CRCONFIG
+
     def __init__(self, cmd, xml_file, xsd_validator, dist_call, **kwargs):
         dmci.CONFIG = CRCONFIG
         super().__init__(cmd, xml_file, xsd_validator, **kwargs)
@@ -147,6 +272,9 @@ app.conf.update(task_serializer='json',
                 result_expires=43200
                 )
 
+
+if not CRCONFIG.readConfig(configFile=os.environ.get("DMCI_CONFIG", None)):
+    sys.exit(1)
 
 """Initialize global xsd_obj used by dmci distributors"""
 XSD_OBJ = None
@@ -395,11 +523,6 @@ def loadFile(filename):
 @app.task(trail=True)
 def dmci_dist_ingest_task(mmd_path, action, call_distributors):
     """Celery task ingesting one mmd file"""
-    CRCONFIG = CRConfig()
-    if not CRCONFIG.readConfig(configFile=os.environ.get("DMCI_CONFIG", None)):
-        sys.exit(1)
-    logger.debug(f"Solr url after config read: {CRCONFIG.solr_service_url}")
-
     data = loadFile(mmd_path)
     status = False
     dmci.CONFIG = CRCONFIG
